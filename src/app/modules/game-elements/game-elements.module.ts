10878cba06c15911f13767a0527626b697b759be
Utente <canzaniello.francesco1988@gmail.com>
2025-02-18 11:16:09 +0100

    aggiunti file iniziali ei component

---
+++ src/app/modules/game-elements/game-elements.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GameElementsRoutingModule } from './game-elements-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GameElementsRoutingModule
+  ]
+})
+export class GameElementsModule { }
